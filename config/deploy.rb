ff404bf65677cef0bda236a513048e0ad61fed48
Jeyavel.Natesan <jeyavel.natesan@velankani.com>
2016-12-06 15:05:04 +0530

    Added the cap configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'jel_api'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:jeyavel-velankani/JelAPI.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
